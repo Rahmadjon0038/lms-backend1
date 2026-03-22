5629af1763e540d1eec05955940b9d87c7ed3ad1
Rahmadjon0038 <abdullayevrahmadjon821@gmail.com>
2026-03-22 23:17:57 +0500

    d

--- routes/teacherSalaryRoutes.js
+++ routes/teacherSalaryRoutes.js
@@ -22,6 +22,7 @@ router.get('/payouts', protect, ctrl.getTeacherSalaryPayouts);
 // Summary va close
 router.get('/months/:month_name/teachers/:teacher_id', protect, ctrl.getTeacherMonthSummary);
 router.post('/months/:month_name/teachers/:teacher_id/close', protectAdmin, ctrl.closeTeacherMonth);
+router.post('/months/:month_name/teachers/:teacher_id/reset-payouts', protectAdmin, ctrl.resetTeacherMonthPayouts);
 router.get('/months/:month_name/teachers', protectAdmin, ctrl.getAllTeachersMonthSummary);
 router.get('/months/:month_name/simple-list', protectAdmin, ctrl.getSimpleTeacherSalaryList);
 
