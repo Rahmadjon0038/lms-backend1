29c901a0b35c507b1dfe008b710239e88aa9a5d1
Rahmadjon0038 <abdullayevrahmadjon821@gmail.com>
2026-07-23 21:59:58 +0500

    d

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -476,7 +476,7 @@ router.get("/:student_id/groups", protect, roleCheck(['admin']), studentControll
  *       401:
  *         description: Ruxsat berilmadi (token kerak)
  */
-// Student o'z akkauntini o'chirishi (parol bilan tasdiqlanadi)
+// Student o'z akkauntini o'chirishi (parol ixtiyoriy)
 router.delete("/me/account", protect, roleCheck(['student']), studentController.deleteMyAccount);
 router.get("/my-groups", protect, studentController.getMyGroups);
 router.get("/my-point-reports", protect, studentController.getMyPointReports);
