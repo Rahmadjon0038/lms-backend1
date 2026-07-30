6c0667d873f1a85d5812df1ef0c5cee64330a3ba
Rahmadjon0038 <abdullayevrahmadjon821@gmail.com>
2026-07-30 18:34:18 +0500

    d

--- routes/teacherStatisticsRoutes.js
+++ routes/teacherStatisticsRoutes.js
@@ -6,13 +6,14 @@ const { roleCheck } = require('../middlewares/roleMiddleware');
 
 const teacherOrAdmin = roleCheck(['teacher', 'admin', 'super_admin']);
 const managerOrAdmin = roleCheck(['english-manager', 'admin', 'super_admin']);
+const managerViewer = roleCheck(['english-manager', 'admin', 'super_admin', 'teacher']);
 
 router.get('/manager/teachers', protect, managerOrAdmin, ctrl.getEnglishManagerTeachers);
 router.get('/manager/reports', protect, managerOrAdmin, ctrl.getManagerDailyStatistics);
 router.get('/groups/:groupId/reports', protect, teacherOrAdmin, ctrl.getGroupStatisticsReports);
 router.post('/lessons/:lessonId', protect, teacherOrAdmin, ctrl.saveLessonStatistics);
 router.put('/lessons/:lessonId', protect, teacherOrAdmin, ctrl.saveLessonStatistics);
-router.get('/lessons/:lessonId', protect, teacherOrAdmin, ctrl.getLessonStatistics);
+router.get('/lessons/:lessonId', protect, managerViewer, ctrl.getLessonStatistics);
 router.delete('/lessons/:lessonId', protect, teacherOrAdmin, ctrl.deleteLessonStatistics);
 
 module.exports = router;
