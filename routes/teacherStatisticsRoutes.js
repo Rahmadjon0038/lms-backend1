5a0c7e0130c6a296d376512f9cb91245e7a2870f
Rahmadjon0038 <abdullayevrahmadjon821@gmail.com>
2026-07-31 10:26:04 +0500

    d

--- routes/teacherStatisticsRoutes.js
+++ routes/teacherStatisticsRoutes.js
@@ -10,6 +10,7 @@ const managerViewer = roleCheck(['english-manager', 'admin', 'super_admin', 'tea
 
 router.get('/manager/teachers', protect, managerOrAdmin, ctrl.getEnglishManagerTeachers);
 router.get('/manager/reports', protect, managerOrAdmin, ctrl.getManagerDailyStatistics);
+router.get('/manager/months', protect, managerOrAdmin, ctrl.getEnglishManagerAvailableMonths);
 router.get('/groups/:groupId/reports', protect, teacherOrAdmin, ctrl.getGroupStatisticsReports);
 router.post('/lessons/:lessonId', protect, teacherOrAdmin, ctrl.saveLessonStatistics);
 router.put('/lessons/:lessonId', protect, teacherOrAdmin, ctrl.saveLessonStatistics);
