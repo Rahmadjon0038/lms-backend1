9d38e4c5f6a6c03cb736c59bffa9389039ffaf66
Rahmadjon0038 <abdullayevrahmadjon821@gmail.com>
2026-08-06 15:38:32 +0500

    d

--- routes/teacherLateRoutes.js
+++ routes/teacherLateRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/teacherLateController');
-const { protectAdmin } = require('../middlewares/authMiddleware');
+const { protect } = require('../middlewares/authMiddleware');
+const { roleCheck } = require('../middlewares/roleMiddleware');
 
 // Tanlangan oy uchun barcha o'qituvchilar + kechikishlari
-router.get('/months/:month_name/teachers', protectAdmin, ctrl.getMonthLateByTeachers);
+router.get('/months/:month_name/teachers', protect, roleCheck(['admin', 'super_admin', 'english-manager']), ctrl.getMonthLateByTeachers);
 
 // Kechikish qo'shish / o'chirish
-router.post('/', protectAdmin, ctrl.createLateRecord);
-router.delete('/:id', protectAdmin, ctrl.deleteLateRecord);
+router.post('/', protect, roleCheck(['admin', 'super_admin', 'english-manager']), ctrl.createLateRecord);
+router.delete('/:id', protect, roleCheck(['admin', 'super_admin', 'english-manager']), ctrl.deleteLateRecord);
 
 module.exports = router;
